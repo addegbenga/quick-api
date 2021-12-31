6f7302cc5147401b08004a9b0f13bb7d7764afd1
addegbenga <addegbenga@gmail.com>
2021-12-31 07:05:28 -0500

    changes to users route

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,8 +1,9 @@
 const User = require('../models/User')
 
 exports.getUserById = async (req, res) => {
+  const name = req.params.name
   try {
-    const user = await User.findById(req.params.userId)
+    const user = await User.find({ username: name })
     return res.json(user)
   } catch (error) {
     console.log(error)
